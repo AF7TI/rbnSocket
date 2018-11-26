abe0081621a13452079a2ffa143bc36c7e87ed0a
matt <AF7TI@users.noreply.github.com>
2018-11-25 18:58:53 -0800

    Update application.py

--- application.py
+++ application.py
@@ -1,82 +1,16 @@
-"""
-Demo Flask application to test the operation of Flask with socket.io
-
-30th May 2014
-
-===================
-
-Updated 13th April 2018
-
-+ Upgraded code to Python 3
-+ Used Python3 SocketIO implementation
-+ Updated CDN Javascript and CSS sources
-
-===================
-
-Repurposed 4th September 2018
-
-+ Added eventlet usage
-+ Added telnet example
-
-"""
-import os, time
-
-def daemon(func):
-    def wrapper(*args, **kwargs):
-        if os.fork(): return
-        func(*args, **kwargs)
-        os._exit(os.EX_OK)
-    return wrapper
-
-# Start with a basic flask app webpage.
-from flask_socketio import SocketIO, emit
-from flask import Flask, render_template, url_for, copy_current_request_context
-from time import sleep
+from websocket_server import WebsocketServer
+import threading
 from threading import Thread, Event
-import eventlet
-eventlet.monkey_patch()
-
-__author__ = 'af7ti'
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-app.config['DEBUG'] = False
-#turn the flask app into a socketio app
-socketio = SocketIO(app, async_mode='eventlet') #"""use when eventlet is installed"""
-#socketio = SocketIO(app') """use when eventlet not installed"""
-
-#telnet Line Reader Thread
-#thread = Thread(target=background_thread)
-#thread2 = Thread(target=background_thread2)
-#thread_stop_event = Event()
-
 
 #telnet
 import telnetlib
 HOST = 'telnet.reversebeacon.net'
 port1 = 7000
 port2 = 7001
-user = 'XXXXX'
+user = 'xxxx'
 
-#tn = telnetlib.Telnet(HOST, port=port1, timeout=1)
-#tn.read_until(b'Please enter your call:')
-#tn.write(user.encode('ascii') + b"\n")
-
-#tn2 = telnetlib.Telnet(HOST, port=port2, timeout=1)
-#tn2.read_until(b'Please enter your call:')
-#tn2.write(user.encode('ascii') + b"\n")
-
-'''child = pexpect.spawn('telnet telnet.reversebeacon.net 7001')
-child2 = pexpect.spawn('telnet telnet.reversebeacon.net 7000')
-child.expect ('Please enter your call:')
-child2.expect ('Please enter your call:')
-child.sendline ('AF7TI')
-child2.sendline ('AF7TI')
-child.maxsize = 1 #turns off buffering
-child2.maxsize = 1 #turns off buffering
-timeout = None
-'''
 def background_thread(): #"""use when eventlet is installed"""    
+    print("in background thread");
     tn = telnetlib.Telnet(HOST, port=port1)
     tn.read_until(b'Please enter your call:')
     tn.write(user.encode('ascii') + b"\r\n")
@@ -84,8 +18,8 @@ def background_thread(): #"""use when eventlet is installed"""
     while True:
         line = tn.read_until(b'\n')  # Read one line
         if line != '':
-              print (line)
-              socketio.emit('newline', {'line': line}, namespace='/test')
+              #print (line)
+              server.send_message_to_all(line)
 
 def background_thread2(): #"""use when eventlet is installed"""
     tn2 = telnetlib.Telnet(HOST, port=port2)
@@ -95,73 +29,55 @@ def background_thread2(): #"""use when eventlet is installed"""
     while True:
         line2 = tn2.read_until(b'\n')  # Read one line
         if line2 != '':
-              print (line2)
-              socketio.emit('newline', {'line': line2}, namespace='/test')
+             #print (line2)
+             server.send_message_to_all(line2)
 
 #telnet Line Reader Thread
 thread = Thread(target=background_thread)
 thread2 = Thread(target=background_thread2)
 
-class TelnetThread(Thread): #"""use when eventlet not installed"""
-    def __init__(self):
-        #self.delay = 1
-        super(TelnetThread, self).__init__()
 
-    def telnetLineReader(self):
-        """
-        Emit input to a socketio instance (broadcast)
-        Ideally to be run in a separate thread?
-        """
-        #infinite loop of readLine attempts
-        print("Emitting spots")
-        while not thread_stop_event.isSet():
-            line = child.readline()
-            if line != '':
-              #print (line)
-              socketio.emit('newline', {'line': line}, namespace='/test')
-            #sleep(self.delay)
+# Called for every client connecting (after handshake)
+def new_client(client, server):
+    print("New client connected and was given id %d" % client['id'])
+    server.send_message_to_all("Hey all, a new client has joined us")
 
-    def run(self):
-        self.telnetLineReader()
+    #Start telnet threads only if the thread has not been started
 
-@app.route('/')
-def index():
-    #only by sending this page first will the client be connected to the socketio instance
-    return render_template('index.html')
-
-@socketio.on('connect', namespace='/test')
-def test_connect():
-    # need visibility of the global thread object
     global thread
     print('Client connected')
     #with thread_lock:
 
-    #Start thread only if the thread has not been started before.
     if not thread.isAlive():
         print("Starting Thread")
-        thread = socketio.start_background_task(target=background_thread) #"""use when eventlet is installed""" 
+        thread = threading.Thread(target=background_thread) #"""use when eventlet is installed""" 
         #thread = TelnetThread() """use when eventlet not installed"""
-        #thread.start() """use when eventlet not installed"""
+        thread.start() # """use when eventlet not installed"""
 
 
     global thread2
     #with thread_lock:
     if not thread2.isAlive():
         print("Starting Thread2")
-        thread2 = socketio.start_background_task(target=background_thread2) #"""use when eventlet is installed""" 
+        thread2 = threading.Thread(target=background_thread2) #"""use when eventlet is installed"""
+        thread2.start() # """use when eventlet not installed"""
 
-@socketio.on('disconnect', namespace='/test')
-def test_disconnect():
-    print('Client disconnected')
+# Called for every client disconnecting
+def client_left(client, server):
+        print("Client(%d) disconnected" % client['id'])
 
-#if __name__ == '__main__':
-#    #socketio.run(app)
-#    socketio.run(app, host='0.0.0.0', port=5000)
 
-@daemon
-def my_func():
-    #if __name__ == '__main__':
-    #socketio.run(app)
-    socketio.run(app, host='0.0.0.0', port=5001)
+# Called when a client sends a message
+def message_received(client, server, message):
+        if len(message) > 200:
+                message = message[:200]+'..'
+        print("Client(%d) said: %s" % (client['id'], message))
 
-my_func()
+
+PORT=5001
+host="0.0.0.0"
+server = WebsocketServer(PORT, host)
+server.set_fn_new_client(new_client)
+server.set_fn_client_left(client_left)
+server.set_fn_message_received(message_received)
+server.run_forever()

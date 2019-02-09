e5b99f4d3c47a10bcc3d940adf6c69c7ec6b3cbb
matt <AF7TI@users.noreply.github.com>
2019-02-09 11:52:41 -0800

    Update application.py

--- application.py
+++ application.py
@@ -168,7 +168,7 @@ def message_received(client, server, message):
 
 PORT=5001
 host="0.0.0.0"
-server = WebsocketServer(PORT, host="0.0.0.0")
+server = WebsocketServer(PORT, host="0.0.0.0", loglevel=logging.INFO)
 server.set_fn_new_client(new_client)
 #server.set_fn_client_left(client_left)
 server.set_fn_message_received(message_received)
